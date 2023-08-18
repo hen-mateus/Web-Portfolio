0f3d0cebc71247f48b0fbc76222627f075c6cc34
Henrique Mateus <111023508+Hen-Mateus@users.noreply.github.com>
2023-08-18 22:44:40 +0100

    Update

--- src/view/contact.js
+++ src/view/contact.js
@@ -6,12 +6,12 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 export default function Contact() {
     return (
-        <div className="container contact-container color-white d-flex align-items-center">
-            <div class="flex-column">
-                <p className='titulo contact-bmargin'>Contact</p>
-                <p className='contact-bmargin-2'>I'm currently looking for opportunities. Whether its a Full-Time Job or a Freelance work. Send me a message, I'll try to get back to you as soon as possible.</p>
-                <a href="mailto:henriquemateus.prof@gmail.com" className='border border-white rounded bg-transparent box-effect text-decoration-none text-reset'><FontAwesomeIcon icon={faEnvelope} /> E-mail</a>
-            </div>
-        </div >
+            <div className="container contact-container color-white d-flex align-items-center">
+                <div class="flex-column">
+                    <p className='titulo contact-bmargin'>Contact</p>
+                    <p className='contact-bmargin-2'>I'm currently looking for opportunities. Whether its a Full-Time Job or a Freelance work. Send me a message, I'll try to get back to you as soon as possible.</p>
+                    <a href="mailto:henriquemateus.prof@gmail.com" className='border border-white box-effect rounded bg-transparent box-effect text-decoration-none text-reset py-3 px-5'><FontAwesomeIcon icon={faEnvelope} className='me-2'/> E-mail</a>
+                </div>
+            </div >
     );
 }
